9314475735577cb3a14b10aab81a1c1f78fa765c
samwanyua <samexample8@gmail.com>
2026-01-21 11:33:41 +0300

    Refresher on types

--- types.js
+++ types.js
@@ -1,3 +1,8 @@
 0 == false // true
 0 === false // false
 
+/*
+JS falsy values (when converted to boolean): false, 0, -0, "", null, undefined, NaN
+Truthy values: non-empty string, numbers other than 0,  Objects {}, [], functions, dates
+
+*/
