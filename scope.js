2f7aa42b49d8a28650e246c80515a4046fc527ac
samwanyua <samexample8@gmail.com>
2026-01-21 11:16:48 +0300

    Refresher on scope

---
+++ scope.js
@@ -0,0 +1,15 @@
+let globalVar= "I am global";
+console.log(globalVar) // works
+
+function testScope(){
+    let functionVar = "I am inside the function"; // works
+    if(true){
+        let blockVar = "I am inside the block";
+        console.log(blockVar)
+    }
+    // console.log(blockVar) // doesn't work
+    console.log(functionVar);
+}
+
+testScope()
+// console.log(functionVar) // doesn't work
