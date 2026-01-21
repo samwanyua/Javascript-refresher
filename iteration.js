889419a5959c6826ce86462e5654b8c2295ffb59
samwanyua <samexample8@gmail.com>
2026-01-21 10:28:02 +0300

    Refresher on forEach

---
+++ iteration.js
@@ -0,0 +1,9 @@
+// for each - returns each item but not a new array
+const properties = ["Hazina", "KCB Towers", "Lonrho Towers", "I&M", 
+    "Nyati House"
+]
+
+properties.forEach(property => {
+    console.log(`We manage ${property}`)
+});
+
