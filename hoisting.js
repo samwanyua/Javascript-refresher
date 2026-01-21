6efb1fee4e5b32ae99e12e1e2a6a62ceb10abca3
samwanyua <samexample8@gmail.com>
2026-01-21 11:27:22 +0300

    Refresher on hoisting

---
+++ hoisting.js
@@ -0,0 +1,26 @@
+// Hoisting in JavaScript is the behavior where variable and function declarations are moved to the top of their scope before code execution. 
+sayHello() // works
+
+function sayHello(){
+    console.log("Hello");
+}
+
+console.log(x) // undefined
+var x = 5
+console.log(x) // works
+
+
+console.log(y) // reference error
+let y = 10;
+
+console.log(z) // reference error
+const z = 10;
+
+// function expressions are like variables, you can't call it before assignment
+sayHi() // type error
+var sayHi = function() {
+    console.log("Hi")
+}
+
+// declarations are hoisted, initializations are not
+// function declarations are fully hoisted - you can call it before it appears in the code
